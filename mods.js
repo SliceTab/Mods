76a6c950879c645575e84f2d94e08560d5491dfe
SliceTab <113535664+SliceTab@users.noreply.github.com>
2023-05-23 11:38:12 +0200

    Update to support the new tab

--- mods.js
+++ mods.js
@@ -3,18 +3,18 @@
 // @version      1.2
 // @description  Slice Tab mods
 // @require      http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js
-// @match        https://sli.ce.it/tab*
+// @match        https://tab.sli.ce.it/*
 // @grant        none
 // ==/UserScript==
 // Settings //
     // Enabled/Disable features //
         var replace_image = true // true = on // false = off //
-        var searchbar = true // true = on // false = off //
+        var hideSearchbar = false // true = on // false = off //
         var darkmode = true
-        var hideSlices = true
-        var hideFavorites = true
+        var hideSlices = false
+        var hideFavorites = false
     // Configuration //
-        var new_image = "Your image" // Must be a link to a direct image! //
+        var new_image = "New image url here!" // Must be a link to a direct image! //
 // End of settings (Make sure to save!) //
 
 
@@ -26,57 +26,33 @@
 /*globals $*/
 $(document).ready(function(){
     if(hideSlices) {
-    $(".user-stats").css("display","none")
+        $(".text-white").css("display", "none")
+        $("img[src='/static/media/sliceLogoLineBaseWhite.8119306a15b60d2319c77c3ccc6b4ba7.svg']").css("display", "none")
     };
-    if(hideFavorites) {
-    const interval = setInterval(() => {
-    if($(".item").length > 0) {
-        $(".widgets .favorites .grid").css("display","none")
-        $(".widgets .favorites").css("display","none")
-        clearInterval(interval)
+    if(hideSearchbar) {
+        $(".bg-white").css("display", "none")
+        $("img[src='/static/media/searchLineGray500.3c25d17cf98eba1417d45aaa4d38eef8.svg']").css("display", "none")
     }
-    }, 100)
+    if(hideFavorites) {
+        $(".swiper").css("display","none")
     };
     if (replace_image == true){
-        $("img[class='js-wallpaper']").attr("src", new_image)
-    };
-    if (searchbar == true){
-        if(!darkmode) {
-        $("body").append ( `
-            <div style="position:fixed;bottom: 75%;display:flex;justify-content:center;background:#ffffff00;margin:0;z-index:4; width: 100%; height: 2.5rem;">
-                <form action="https://www.google.com/search" method="get" id="search-form">
-                <div style="display: flex; width: 100%;">
-                    <input name="q" type="text" placeholder="Search on google..." autocomplete="off" autofocus style="color: black; width: 75vh; height: 2.5rem; border-radius: 100rem; padding-left: 0.75rem; padding-right: 0.75rem; padding-top: 0.25rem; padding-bottom: 0.25rem; border-color: black; background-color: white;">
-                </div>
-                </form>
-            </div>
-        ` );
-    } else {
-        $("body").append ( `
-            <div style="position:fixed;bottom: 75%;display:flex;justify-content:center;background:#ffffff00;margin:0;z-index:4; width: 100%; height: 2.5rem;">
-                <form action="https://www.google.com/search" method="get" id="search-form">
-                <div style="display: flex; width: 100%;">
-                    <input name="q" type="text" placeholder="Search..." autocomplete="off" autofocus style="color: white; width: 75vh; height: 2.5rem; border-radius: 100rem; padding-left: 0.75rem; padding-right: 0.75rem; padding-top: 0.25rem; padding-bottom: 0.25rem; border-color: white; background-color: black;">
-                </div>
-                </form>
-            </div>
-        ` );
-        };
+        $("img[alt='Slice New Tab Wallpaper']").attr("src", new_image)
+        // For the image by
+        $(".text-primary-100").css("display", "none")
+        $(".text-text-sm").css("display", "none")
+        //
     };
     if (darkmode == true) {
-        $(".time").css("color", "black")
-        $(".js-slices").css("color", "black")
-        $(".body p").css("color", "black")
-        $(".head svg path").css("fill", "black")
-        addGlobalStyle('.widgets .favorites h1 { font-weight: 600; font-size: 18px; line-height: 28px; color: black; }');
-        const interval = setInterval(() => {
-        if($(".item").length > 0) {
-        $(".item p").css("color", "black")
-        $(".item .image").css("background-color", "black")
-        $(".item .image p").css("color", "white")
-        clearInterval(interval)
-        }
-        }, 100)
+        // Slices
+        $(".text-white").css("color", "black")
+        $("img[src='/static/media/sliceLogoLineBaseWhite.8119306a15b60d2319c77c3ccc6b4ba7.svg']").attr("src", "https://i.imgur.com/iBkZMuC.png")
+        // Search bar
+        $(".bg-white").css("background-color", "black")
+        $(".bg-white").css("color", "white")
+        // Options
+        $("img[src='/static/media/settingAdjustLineBaseWhite.09146b837dfc4a489bf95bbaec349eca.svg']").attr("src", "https://i.imgur.com/NtpPEe8.png")
+        addGlobalStyle('.widgets .favorites h1 { font-weight: 600; font-size: 18px; line-height: 28px; color: black; }')
     };
 });
 
